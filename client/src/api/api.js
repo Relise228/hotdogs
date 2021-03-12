14820a07f601af1515a1b0916dbd346ce37fdd95
Relise228 <andreysmit43@gmail.com>
2021-03-12 14:45:13 +0200

    make it better

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5000"
+    baseURL: "/"
 })
 
 export const hotAPI = {
-    getHotDogs: () => instance.get('/').then(response => response.data),
-    updateHotDog: (id, img, name, price, description) => instance.post('/hotdog/update', {
+    getHotDogs: () => instance.get('').then(response => response.data),
+    updateHotDog: (id, img, name, price, description) => instance.post('hotdog/update', {
         id, img, name, price, description
     }).then(response => response.data),
-    deleteHotDog: (id) => instance.delete('/hotdog/delete', {data: {id}}).then(response => response.data),
-    addHotDog: (img, name, price, description) => instance.put('/hotdog/add', {
+    deleteHotDog: (id) => instance.delete('hotdog/delete', {data: {id}}).then(response => response.data),
+    addHotDog: (img, name, price, description) => instance.put('hotdog/add', {
         img, name, price, description
     }).then(response => response.data),
 }
